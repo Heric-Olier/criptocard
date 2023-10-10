e7c55cd6e56dbba497d3cc1b44e29ee497b35cf3
imchristiandev <imchristiandev@gmail.com>
2023-10-09 19:44:37 -0500

    feat: adaptation of context logic into countries selection

--- components/SelectComponent.tsx
+++ components/SelectComponent.tsx
@@ -1,178 +1,48 @@
 import React, { use, useContext, useEffect, useState } from "react";
+import Image from "next/image";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import { ChevronUpIcon } from "@heroicons/react/24/solid";
 import styles from "./select.module.scss";
 import axios from "axios";
 import CardProfileContext from "@/app/context/cardProfileContext";
-import CountryOrigin from './CountryOrigin';
-
-const countries = [
-  {
-    key: "MX",
-    label: "Mexico",
-    img: "https://flagicons.lipis.dev/flags/4x3/mx.svg",
-  },
-  {
-    key: "US",
-    label: "United States",
-    img: "https://flagicons.lipis.dev/flags/4x3/us.svg",
-  },
-  {
-    key: "CA",
-    label: "Canada",
-    img: "https://flagicons.lipis.dev/flags/4x3/ca.svg",
-  },
-  {
-    key: "CO",
-    label: "Colombia",
-    img: "https://flagicons.lipis.dev/flags/4x3/co.svg",
-  },
-  {
-    key: "AR",
-    label: "Argentina",
-    img: "https://flagicons.lipis.dev/flags/4x3/ar.svg",
-  },
-  {
-    key: "PE",
-    label: "Peru",
-    img: "https://flagicons.lipis.dev/flags/4x3/pe.svg",
-  },
-  {
-    key: "VE",
-    label: "Venezuela",
-    img: "https://flagicons.lipis.dev/flags/4x3/ve.svg",
-  },
-  {
-    key: "BR",
-    label: "Brazil",
-    img: "https://flagicons.lipis.dev/flags/4x3/br.svg",
-  },
-  {
-    key: "CL",
-    label: "Chile",
-    img: "https://flagicons.lipis.dev/flags/4x3/cl.svg",
-  },
-  {
-    key: "EC",
-    label: "Ecuador",
-    img: "https://flagicons.lipis.dev/flags/4x3/ec.svg",
-  },
-  {
-    key: "GT",
-    label: "Guatemala",
-    img: "https://flagicons.lipis.dev/flags/4x3/gt.svg",
-  },
-  {
-    key: "CU",
-    label: "Cuba",
-    img: "https://flagicons.lipis.dev/flags/4x3/cu.svg",
-  },
-  {
-    key: "BO",
-    label: "Bolivia",
-    img: "https://flagicons.lipis.dev/flags/4x3/bo.svg",
-  },
-  {
-    key: "DO",
-    label: "Dominican Republic",
-    img: "https://flagicons.lipis.dev/flags/4x3/do.svg",
-  },
-  {
-    key: "HN",
-    label: "Honduras",
-    img: "https://flagicons.lipis.dev/flags/4x3/hn.svg",
-  },
-  {
-    key: "PY",
-    label: "Paraguay",
-    img: "https://flagicons.lipis.dev/flags/4x3/py.svg",
-  },
-  {
-    key: "SV",
-    label: "El Salvador",
-    img: "https://flagicons.lipis.dev/flags/4x3/sv.svg",
-  },
-  {
-    key: "NI",
-    label: "Nicaragua",
-    img: "https://flagicons.lipis.dev/flags/4x3/ni.svg",
-  },
-  {
-    key: "CR",
-    label: "Costa Rica",
-    img: "https://flagicons.lipis.dev/flags/4x3/cr.svg",
-  },
-  {
-    key: "PR",
-    label: "Puerto Rico",
-    img: "https://flagicons.lipis.dev/flags/4x3/pr.svg",
-  },
-  {
-    key: "PA",
-    label: "Panama",
-    img: "https://flagicons.lipis.dev/flags/4x3/pa.svg",
-  },
-  {
-    key: "UY",
-    label: "Uruguay",
-    img: "https://flagicons.lipis.dev/flags/4x3/uy.svg",
-  },
-  {
-    key: "JM",
-    label: "Jamaica",
-    img: "https://flagicons.lipis.dev/flags/4x3/jm.svg",
-  },
-  {
-    key: "TT",
-    label: "Trinidad and Tobago",
-    img: "https://flagicons.lipis.dev/flags/4x3/tt.svg",
-  },
-  {
-    key: "BS",
-    label: "Bahamas",
-    img: "https://flagicons.lipis.dev/flags/4x3/bs.svg",
-  },
-  {
-    key: "BB",
-    label: "Barbados",
-    img: "https://flagicons.lipis.dev/flags/4x3/bb.svg",
-  },
-  {
-    key: "HT",
-    label: "Haiti",
-    img: "https://flagicons.lipis.dev/flags/4x3/ht.svg",
-  },
-  {
-    key: "GY",
-    label: "Guyana",
-    img: "https://flagicons.lipis.dev/flags/4x3/gy.svg",
-  },
-  {
-    key: "BZ",
-    label: "Belize",
-    img: "https://flagicons.lipis.dev/flags/4x3/bz.svg",
-  },
-];
 
 export default function SelectComponent() {
+  const [countries, setCountries] = useState([]);
   const [openSelect, setOpenSelect] = useState(false);
-  const [selectedCountry, setSelectedCountry] = useState({
-    label: "Select Country",
-    img: "",
-  });
-  const context = useContext(CardProfileContext)
+  const [selectedCountry, setSelectedCountry] = useState<any>()
 
-  console.log(CountryOrigin)
+  const context:any = useContext(CardProfileContext)
+
+  console.log("My context", context?.id, typeof(context?.id))
+
   useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all')
-      .then(response => console.log(response.data))
+    axios.get('http://cryptomex-prod.eastus.cloudapp.azure.com:8000/api/countries/')
+      .then(response => setCountries(response.data))
       .catch(error => {
-        console.error("This is an error", error.status)
+        console.error("This is an error", error.status);
       })
   }, [])
 
-  // const { userData, setUserData } = useGlobalContext();
+  
+  const updateCountry = ( country: any ) => {
+    setSelectedCountry(country)
+    setOpenSelect(!openSelect)
+    context?.setCountryOrigin( country.id )
+  } 
 
+  const renderCountries = () => {
+    return countries.map((country: any) => (
+      <div
+        key={country?.id}
+        className={styles.select__option}
+        onClick={() => updateCountry(country)}
+      >
+        <Image src={country?.icon_name} alt={country?.name} width={20} height={15} />
+        
+        <p>{country?.name}</p>
+      </div>
+    ))
+  }
   return (
     <article className={styles.select__container}>
       <button
@@ -180,13 +50,18 @@ export default function SelectComponent() {
         onClick={() => setOpenSelect(!openSelect)}
       >
         <div className={styles.countryAnd__flag}>
-          {selectedCountry.img && (
-            <img
-              className={styles.country__flag}
-              src={selectedCountry.img}
-              alt={selectedCountry.label}
-            />
-          )}
+          {
+            selectedCountry ? (
+              <div
+                key={selectedCountry?.id}
+                className={styles.select__option}
+              >
+                <Image src={selectedCountry?.icon_name} alt={selectedCountry?.name} width={20} height={15} />
+                
+                <p>{selectedCountry?.name}</p>
+              </div>
+            ) : 'Choose here'
+          }
           {/* <h3>{userData.country_origin}</h3> */}
         </div>
 
@@ -198,26 +73,7 @@ export default function SelectComponent() {
       </button>
       {openSelect && (
         <div className={styles.select__optionsContainer}>
-          {countries.map((country, index) => (
-            <div
-              key={index}
-              className={styles.select__option}
-              // onClick={() => {
-              //   setUserData((prev) => ({
-              //     ...prev,
-              //     country_origin: country.label,
-              //   }));
-              //   setOpenSelect(false);
-              // }}
-            >
-              <img
-                className={styles.country__flag}
-                src={country.img}
-                alt={country.label}
-              />
-              <p>{country.label}</p>
-            </div>
-          ))}
+          {renderCountries()}
         </div>
       )}
     </article>
